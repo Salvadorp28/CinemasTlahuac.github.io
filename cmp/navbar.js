2eb2fcde5fb6c50f7ed43fbe361d6a904bcbebe2
CinemasTlahuac <131704561+CinemasTlahuac@users.noreply.github.com>
2023-05-01 13:50:34 -0600

    Navbar Inicial

---
+++ cmp/navbar.js
@@ -0,0 +1,23 @@
+class Navbar extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = /* html */
+            `
+        <nav>
+            <a href = "index.html"><img src="images/Logo.png" alt="Logo"></a>
+            <ul>
+                <li><a href="cartelera.html">Cartelera</a></li>
+            </ul>
+            <ul>
+                <li><a href="index.html">Inicio de Sesión</a></li>
+            </ul>
+            <form>
+                <input type="text" placeholder="Buscar">
+                <input type="submit" value="Buscar">
+            </form>
+        </nav>
+
+        `;
+    }
+}
+
+customElements.define("navbar", Navbar);
